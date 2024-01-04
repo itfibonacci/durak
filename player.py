7f89d169df118fb475ace7c4219ef65308a0afa9
itfibonacci <levon.tumanyan@outlook.com>
2024-01-03 19:23:59 -0500

    moved players list inside player class

--- player.py
+++ player.py
@@ -4,10 +4,13 @@ from random import choice
 # each player has the cards in their hand
 
 class Player():
+	players = []
+
 	def __init__(self, position) -> None:
 		self.name = choice(self.first_names)	
 		self.position = position
 		self.hand = []
+		Player.players.append(self)
 		logging.info(f'Created a player object with Name: {self.name}, Position: {self.position}')
 		
 	def __str__(self) -> str:
